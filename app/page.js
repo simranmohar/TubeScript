f1e41e066460e83dae598a2b35ec4f5e7c372f8f
Simran Mohar <77524871+simranmohar@users.noreply.github.com>
2025-10-13 18:48:02 -0700

    structure and basic style setup

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold">TubeScript</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold">TubeScript</h1>
+      <Button>Get Started</Button>
     </main>
   );
 }
